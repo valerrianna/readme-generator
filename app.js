f842333fa11ac3df2e4aa8696479d3a57c8b6efd
Valerie <valerie.tram.anh.n@gmail.com>
2021-10-16 17:01:08 -0400

    add more prompt questions

--- app.js
+++ app.js
@@ -9,12 +9,49 @@ const fs = require('fs');
 //     console.log('Readme complete! Check out your readme.md to see the output!');
 //   });
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the title of your project?'
-    }
-  ])
-  .then(answers => console.log(answers));
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project.'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please explain the installation process.'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Are there any contributing guidelines?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Are there any test instructions?'
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+      }
+    ]);
+  };
+  
+  promptUser().then(answers => console.log(answers));
