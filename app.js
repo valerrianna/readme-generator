bf0e1f9b45475d3920f8b255888197cb18947b86
Valerie <valerie.tram.anh.n@gmail.com>
2021-10-16 14:32:10 -0400

    add and test inquirer functions

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const readMe = generatePage(title,github);
 
-const [title, github] = profileDataArgs;
+// fs.writeFile('./sampleREADME.md', readMe, err => {
+//     if (err) throw err;
+//     console.log('Readme complete! Check out your readme.md to see the output!');
+//   });
 
-
-fs.writeFile('./sampleREADME.md', generatePage(title, github), err => {
-    if (err) throw err;
-    console.log('Readme complete! Check out your readme.md to see the output!');
-  });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the title of your project?'
+    }
+  ])
+  .then(answers => console.log(answers));
